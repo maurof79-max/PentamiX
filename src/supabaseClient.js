50abb19c68ada5bd26ad11afce8dc970a461477c
maurof79-max <maurof79@gmail.com>
2026-01-10 09:27:32 +0100

    Vercel update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Sostituisci con i tuoi dati reali da Supabase -> Settings -> API
-const supabaseUrl = 'https://mqdpojtisighqjmyzdwz.supabase.co';
-const supabaseKey = 'sb_publishable_jNkRkb1bF-N6654An9dIvA_apAUtvSc'; // Quella che inizia con eyJ...
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
